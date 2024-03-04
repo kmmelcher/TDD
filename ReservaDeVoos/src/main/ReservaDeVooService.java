6d6f3411d0b5b919bd180f8b649522fffaf4eca7
Vinícius Azevedo <sousa0240@gmail.com>
2024-03-04 17:41:37 -0300

    implementa funcionalidade de reserva de voos

---
+++ ReservaDeVoos/src/main/ReservaDeVooService.java
@@ -0,0 +1,28 @@
+package main;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReservaDeVooService {
+
+    private List<Voo> voos;
+
+    public ReservaDeVooService() {
+        this.voos = new ArrayList<>();
+    }
+
+    public boolean adicionaVoo(Voo voo) {
+        return voos.add(voo);
+    }
+
+    public boolean reservaVoo(int id, int assento) {
+        for (Voo vooCadastrado : voos) {
+            if (vooCadastrado.getId() == id) {
+                vooCadastrado.getAssentos()[assento-1] = true;
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
