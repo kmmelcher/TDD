6b79d6ae80f29541d8a2803561e540d3ce1c46f2
Vinícius Azevedo <sousa0240@gmail.com>
2024-03-04 22:46:52 -0300

    Adiciona suite de testes a serem implementados

--- ReservaDeVoos/src/tests/ControllerTest.java
+++ ReservaDeVoos/src/tests/ControllerTest.java
@@ -33,6 +33,8 @@ public class ControllerTest {
     @Test
     public void testReservarAssento() {
         Assertions.assertTrue(reservaDeVooService.reservaVoo(1, 12));
+        // FIXME Os usuários devem ser capazes de selecionar um voo e reservá-lo,
+        //  inserindo detalhes como nome, número de passageiros e informações de contato.
     }
 
     @Test
@@ -63,7 +65,32 @@ public class ControllerTest {
                         "Origem: Aeroporto X de Brasília\n" +
                         "Destino: Aeroporto Y de Salvador\n" +
                         "Preço R$1200\n" +
-                        "Capacidade: 200 passageiros",
+                        "Capacidade: 200 passageiros", // FIXME incluir assentos disponíveis (x disponíveis)
                 reservaDeVooService.exibeVoo(1));
     }
+
+    /*
+        TODO:
+            1- Adição duplicada de vôo
+            2- exibição de vôo inexistente
+            3- get assentos disponíveis
+            4- get assentos disponíveis (nenhum disponível)
+            5- get Voos disponíveis
+            6- get voos disponíveis (com vôos passados E/OU vôos esgotados presentes)
+            7- Pesquisa por origem
+            8- Pesquisa por origem (nenhum disponível)
+            9- Pesquisa por origem (com destinos sendo a origem)
+            10- Pesquisa por destino
+            11- Pesquisa por destino (nenhum disponível)
+            12- Pesquisa por destino (com origens sendo o destino)
+            13- Pesquisa por data
+            14- Pesquisa por data passada
+            15- Pesquisa por número de passageiros
+            16- Pesquisa por número de passageiros (valor negativo)
+            17- Pesquisa por número de passageiros (valor zero)
+            18- Cancelar Voo (por código de reserva)
+            19- Cancelar Voo (por identificação pessoal)
+            20- Cancelar Voo (codigo inexistente)
+            21- Cancelar Voo (Identificação pessoal inexistente)
+     */
 }
