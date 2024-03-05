8478bfdb64b0db999804704985af2f90de8d69c7
Vinícius Azevedo <sousa0240@gmail.com>
2024-03-05 16:09:57 -0300

    inclui testes de pesquisa por data

--- ReservaDeVoos/src/tests/ControllerTest.java
+++ ReservaDeVoos/src/tests/ControllerTest.java
@@ -19,6 +19,7 @@ public class ControllerTest {
 
         Calendar calendar = Calendar.getInstance();
 
+
         calendar.set(Calendar.YEAR, year);
         calendar.set(Calendar.MONTH, month-1);
         calendar.set(Calendar.DAY_OF_MONTH, day);
@@ -29,6 +30,39 @@ public class ControllerTest {
         calendar.set(Calendar.MILLISECOND, 0);
 
         return calendar.getTime();
+    }
+
+    private void incluiVoos() {
+        reservaDeVooService.adicionaVoo(
+                new Voo(
+                        2,
+                        "Aeroporto A de Belo Horizonte",
+                        "Aeroporto B de Cuiabá",
+                        setTime(21, 12, 2024, 15),
+                        new BigDecimal(20),
+                        20
+                )
+        );
+        reservaDeVooService.adicionaVoo(
+                new Voo(
+                        3,
+                        "Aeroporto A de Brasília",
+                        "Aeroporto B de Cuiabá",
+                        setTime(21, 6, 2024, 15),
+                        new BigDecimal(20),
+                        20
+                )
+        );
+        reservaDeVooService.adicionaVoo(
+                new Voo(
+                        4,
+                        "Aeroporto R de Brasília",
+                        "Aeroporto Y de Cuiabá",
+                        setTime(21, 6, 2020, 15),
+                        new BigDecimal(20),
+                        20
+                )
+        );
     }
 
     @BeforeEach
@@ -293,36 +327,7 @@ public class ControllerTest {
 
     @Test
     void testPesquisaPorOrigem() {
-        reservaDeVooService.adicionaVoo(
-                new Voo(
-                        2,
-                        "Aeroporto A de Belo Horizonte",
-                        "Aeroporto B de Cuiabá",
-                        setTime(21, 12, 2024, 15),
-                        new BigDecimal(20),
-                        20
-                )
-        );
-        reservaDeVooService.adicionaVoo(
-                new Voo(
-                        3,
-                        "Aeroporto A de Brasília",
-                        "Aeroporto B de Cuiabá",
-                        setTime(21, 6, 2024, 15),
-                        new BigDecimal(20),
-                        20
-                )
-        );
-        reservaDeVooService.adicionaVoo(
-                new Voo(
-                        4,
-                        "Aeroporto R de Brasília",
-                        "Aeroporto Y de Cuiabá",
-                        setTime(21, 6, 2020, 15),
-                        new BigDecimal(20),
-                        20
-                )
-        );
+        incluiVoos();
 
         Assertions.assertEquals(
                 "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n" +
@@ -352,36 +357,7 @@ public class ControllerTest {
 
     @Test
     void testPesquisaPorDestino() {
-        reservaDeVooService.adicionaVoo(
-                new Voo(
-                        2,
-                        "Aeroporto A de Belo Horizonte",
-                        "Aeroporto B de Cuiabá",
-                        setTime(21, 12, 2024, 15),
-                        new BigDecimal(20),
-                        20
-                )
-        );
-        reservaDeVooService.adicionaVoo(
-                new Voo(
-                        3,
-                        "Aeroporto A de Brasília",
-                        "Aeroporto B de Cuiabá",
-                        setTime(21, 6, 2024, 15),
-                        new BigDecimal(20),
-                        20
-                )
-        );
-        reservaDeVooService.adicionaVoo(
-                new Voo(
-                        4,
-                        "Aeroporto R de Brasília",
-                        "Aeroporto Y de Cuiabá",
-                        setTime(21, 6, 2020, 15),
-                        new BigDecimal(20),
-                        20
-                )
-        );
+        incluiVoos();
 
         Assertions.assertEquals(
                 "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n" +
@@ -408,11 +384,34 @@ public class ControllerTest {
     void testPesquisaPorDestinoInexistente() {
         Assertions.assertEquals("", reservaDeVooService.pesquisaPorDestino("João Pessoa"));
     }
+
+    @Test
+    void testPesquisaPorData() {
+        incluiVoos();
+
+        Assertions.assertEquals(
+                "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n" +
+                        "== EXIBIÇÃO DE VÔO DE ID 1 ==\n" +
+                        "Origem: Aeroporto X de Brasília\n" +
+                        "Destino: Aeroporto Y de Salvador\n" +
+                        "Preço: R$1200\n" +
+                        "Dia e hora: Sat Dec 21 15:00:00 BRT 2024\n" +
+                        "Capacidade: 200 passageiros\n" +
+                        "(200 assentos disponíveis)\n" +
+                        "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n" +
+                        "== EXIBIÇÃO DE VÔO DE ID 2 ==\n" +
+                        "Origem: Aeroporto A de Belo Horizonte\n" +
+                        "Destino: Aeroporto B de Cuiabá\n" +
+                        "Preço: R$20\n" +
+                        "Dia e hora: Sat Dec 21 15:00:00 BRT 2024\n" +
+                        "Capacidade: 20 passageiros\n" +
+                        "(20 assentos disponíveis)\n",
+                reservaDeVooService.pesquisaPorData(21, 12, 2024)
+        );
+
+    }
     /*
         TODO:
-            10- Pesquisa por destino
-            11- Pesquisa por destino (nenhum disponível)
-            12- Pesquisa por destino (com origens sendo o destino)
             13- Pesquisa por data
             14- Pesquisa por data passada
             15- Pesquisa por número de passageiros
